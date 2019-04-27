71f2ec1f59d5741143949c9e223816d7186e7c9c
Angel Cabrera <soyangelcabrera@gmail.com>
2019-04-27 10:52:03 -0400

    Proyecto PlatziVideo Postcss Version Casi Finalizada

---
+++ postcss.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+	plugins: [
+		require('postcss-import')({
+			plugins: [
+			]
+		}),
+		require('stylelint'),
+		require('postcss-font-magician')({
+			variants: {
+				'Lato': {
+					300: [],
+					400: []
+				}
+			}
+		}),
+		require('postcss-preset-env')({
+			stage: 0,
+			autoprefixer: {
+				grid: true,
+			},
+			preserve: false,
+		}),
+		require('postcss-apply'),
+		require('postcss-color-hwb'),
+	]
+}
